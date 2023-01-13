529194e1cb4196c751d46a40e1f7fe2172fe8ab1
salvi.18188 <salvi.18188@E2-9-PC04>
2023-01-13 13:46:52 +0100

    Aggiungere i file di progetto.

---
+++ ClientREST/ClientREST/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+
+    public class Album
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+
+        override
+        public string ToString()
+        {
+            return "id: " + id + "\ntitle: " + title + "\nuser: " + userId.ToString() + "\ncompleted: " + completed;
+        }
+    }
+
+
+    class Program
+    {
+
+        static HttpClient client = new HttpClient();
+
+        static void ShowProduct(Album album)
+        {
+            Console.WriteLine(album.ToString());
+        }
+
+        static async Task<Album> GetAlbumAsync(string path)
+        {
+            Album product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await JsonSerializer.DeserializeAsync<Album>(await response.Content.ReadAsStreamAsync());
+            }
+            return product;
+        }
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                Album album = null;
+
+                // Get an existing product
+                album = await GetAlbumAsync("/todos/2");
+                ShowProduct(album);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
